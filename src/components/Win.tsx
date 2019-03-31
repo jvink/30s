f67014524510cc9e6a27c4093d617b10617759b4
Jurian Vink <0950140@hr.nl>
2019-03-31 11:42:24 +0200

    fixed balloons

--- src/components/Win.tsx
+++ src/components/Win.tsx
@@ -4,16 +4,20 @@ import { ReactComponent as Balloon2 } from '../resources/balloon2.svg';
 import { ReactComponent as Balloon3 } from '../resources/balloon3.svg';
 import { ReactComponent as Balloon4 } from '../resources/balloon4.svg';
 import { ReactComponent as Balloon5 } from '../resources/balloon5.svg';
+import '../styles/Win.scss';
 
 const Win = () => {
+    let a = [
+        <Balloon1 className="balloons" style={{top: (100*Math.random()) + "%", left: (100*Math.random()) + "%"}}/>,
+        <Balloon2 className="balloons" style={{top: (100*Math.random()) + "%", left: (100*Math.random()) + "%"}}/>,
+        <Balloon3 className="balloons" style={{top: (100*Math.random()) + "%", left: (100*Math.random()) + "%"}}/>,
+        <Balloon4 className="balloons" style={{top: (100*Math.random()) + "%", left: (100*Math.random()) + "%"}}/>,
+        <Balloon5 className="balloons" style={{top: (100*Math.random()) + "%", left: (100*Math.random()) + "%"}}/>
+    ];
+
     return (
         <div>
-            <Balloon1 style={{maxWidth: '200px'}}/>
-            <Balloon2 style={{maxWidth: '200px'}}/>
-            <Balloon3 style={{maxWidth: '200px'}}/>
-            <Balloon4 style={{maxWidth: '200px'}}/>
-            <Balloon5 style={{maxWidth: '200px'}}/>
-            <div className="balloons"></div>
+            {a.map((b) => {return b})}
         </div>
     );
 }
