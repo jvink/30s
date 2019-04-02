1b958d08f39e1a255e6d01f2cc9ce4b988a7fecf
Jurian-PC\Jurian <j.vink@gdksoftware.nl>
2019-04-02 23:19:39 +0200

    fixed

--- README.md
+++ README.md
@@ -6,5 +6,6 @@ TODO:
 - Scrollbar mooier?
 - Teams opslaan in cookies?
 - Ads?
+- talen?
 - Builden en deployen op github pages
 - toasts bug checken https://github.com/Vashnak/react-toasts/issues/9
--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -6,7 +6,7 @@ type Props = {
 }
 
 const Timer = ({doneTimer}: Props) => {
-    const [timerValue, setTimerValue] = useState<number>(333330);
+    const [timerValue, setTimerValue] = useState<number>(3);
 
     useEffect(() => {
         countDownTimer();
