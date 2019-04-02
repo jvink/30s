e97f29511094ae5fab87bfa61050636d3d76729d
Jurian-PC\Jurian <j.vink@gdksoftware.nl>
2019-04-02 14:51:50 +0200

    rewrite a bit

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,10 +2,6 @@ import React from 'react';
 import IMG from '../30s.png';
 import '../styles/Logo.scss';
 
-const Logo = () => (
-    <div className="logo-wrapper">
-        <img className="logo" src={IMG}/>
-    </div>
-);
+const Logo = () => <img className="logo" src={IMG}/>;
 
 export default Logo;
