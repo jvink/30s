99fa99e18245b3a0d8bc966e3c10eb4c1750ea92
Jurian-PC\Jurian <j.vink@gdksoftware.nl>
2019-04-02 14:25:10 +0200

    Added credits

---
+++ src/components/Credit.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Credit.scss';
+
+const Credit = () => (
+    <div className="credit-wrapper">
+        <a className="credit" href="https://jvink.github.io/portfolio/#/" target="_blank" rel="noopener noreferrer">
+            More by Jurian Vink
+        </a>
+    </div>
+);
+
+export default Credit;
