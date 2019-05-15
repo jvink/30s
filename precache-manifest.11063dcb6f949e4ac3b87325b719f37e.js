1a1c1e698d5173bdc09099125094f461e8f8471c
Jurian-PC\Jurian <j.vink@gdksoftware.nl>
2019-05-15 22:00:37 +0200

    Updates

---
+++ precache-manifest.11063dcb6f949e4ac3b87325b719f37e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42f699f6c25eaed4a1bf09590517f649",
+    "url": "/30s/index.html"
+  },
+  {
+    "revision": "56b7c2101c1603b420b9",
+    "url": "/30s/static/css/main.1b3c3696.chunk.css"
+  },
+  {
+    "revision": "f204852bbe3464e3d65c",
+    "url": "/30s/static/js/2.4f24504e.chunk.js"
+  },
+  {
+    "revision": "56b7c2101c1603b420b9",
+    "url": "/30s/static/js/main.a046edb5.chunk.js"
+  },
+  {
+    "revision": "dda3601ee43c6583fb8c",
+    "url": "/30s/static/js/runtime~main.7c10d72c.js"
+  },
+  {
+    "revision": "40c52c0c5d52418f694238e1ef9fd30a",
+    "url": "/30s/static/media/30s.40c52c0c.png"
+  }
+]);
